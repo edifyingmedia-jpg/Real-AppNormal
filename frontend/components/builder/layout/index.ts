dcf479d8b82c2a236af6aa9a048e21091c56b1d1
Tiffany <edifying.media@gmail.com>
2026-06-15 11:23:15 -0400

    Create index.ts

---
+++ frontend/components/builder/layout/index.ts
@@ -0,0 +1,6 @@
+// frontend/components/builder/layout/index.ts
+
+export * from "./BuilderShell";
+export * from "./BuilderMain";
+export * from "./BuilderLeftSidebar";
+export * from "./BuilderRightSidebar";
