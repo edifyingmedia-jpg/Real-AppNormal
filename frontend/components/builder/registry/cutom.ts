47ed253a0aeac971557a52200149d317e5fffaa5
Tiffany <edifying.media@gmail.com>
2026-06-15 18:19:40 -0400

    Delete frontend/components/builder/registry/cutom.ts

--- frontend/components/builder/registry/cutom.ts
+++
@@ -1,7 +0,0 @@
-// frontend/components/builder/registry/custom.ts
-
-import { ComponentDefinition } from "./types";
-
-export const customComponents: ComponentDefinition[] = [
-  // Add custom components here later
-];
