35c8fafc8a34056ec710554f7403a2bcdcbb28ac
Tiffany <edifying.media@gmail.com>
2026-06-15 13:24:15 -0400

    Delete frontend/app/billing directory

--- frontend/app/billing/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function BillingLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <div className="max-w-3xl mx-auto py-12 px-6">
-        {children}
-      </div>
-    </div>
-  );
-}
